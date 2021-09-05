a556c0950d68c75fb5382d78ad393768f933ef87
Márk Polák <polak@cngroup.dk>
2021-09-05 20:30:49 +0200

    Extended tile data

--- lib/ITile.dart
+++ lib/ITile.dart
@@ -9,4 +9,25 @@ class ITile {
   ITile.empty();
 
   ITile(this.size, this.position, this.index);
+
+  int get height {
+    return this.size.height;
+  }
+
+  int get width {
+    return this.size.width;
+  }
+
+  GridPosition get endPosition {
+    return GridPosition(
+        x: this.position.x + this.width, y: this.position.y + this.height);
+  }
+
+  List<double> get xRange {
+    return List<double>.generate(this.width, (index) => index + position.x);
+  }
+
+  List<double> get yRange {
+    return List<double>.generate(this.height, (index) => index + position.y);
+  }
 }
--- lib/TileSize.dart
+++ lib/TileSize.dart
@@ -12,4 +12,27 @@ extension TileSizeExtension on TileSize {
         return 8;
     }
   }
+
+  int get height {
+    switch (this) {
+      case TileSize.S:
+        return 1;
+      case TileSize.M:
+      case TileSize.L:
+      default:
+        return 2;
+    }
+  }
+
+  int get width {
+    switch (this) {
+      case TileSize.S:
+        return 1;
+      case TileSize.M:
+        return 2;
+      case TileSize.L:
+      default:
+        return 4;
+    }
+  }
 }
